c2169269ce06ef280fca0eba6f9f6cf7a0de83af
Nishchey khajuria <nishcheycapture2014@gmail.com>
2025-06-23 22:26:23 +0530

    hash routes added

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -6,7 +6,7 @@ import '../ccss/Modal.css';
 import Loader from '../components/Loader';
 import FeedPostCard from '../components/FeedPostCard';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
 function Modal({ title, list, onClose }) {
   React.useEffect(() => {
